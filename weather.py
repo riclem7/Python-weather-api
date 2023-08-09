83c3a3a4e68623b0d52f7c561ef0949da2b9428d
riclem7 <riclem7@gmail.com>
2023-08-08 18:50:59 -0700

    inital change

---
+++ weather.py
@@ -0,0 +1,15 @@
+import datetime as dt
+import requests
+
+BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
+API_KEY = open('api_key.txt', 'r').read()
+CITY = "Fresno"
+
+url = BASE_URL + "appid=" + API_KEY + "&q=" + CITY
+
+response =  requests.get(url).json()
+
+print (response)
+
+
+
